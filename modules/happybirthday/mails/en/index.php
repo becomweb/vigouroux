cf19c5d6f7ce897cb8788ad646a7ac79c6c7236a
BeComWeb <contact@agence-becomweb.fr>
2018-09-12 14:35:02 +0200

    Ajout repertoire de stockage des mails (fichiers html et txt non pris en compte, voir gitignore)

---
+++ modules/happybirthday/mails/en/index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * HAPPY BIRTHDAY
+ *
+ *    @category front_office_features
+ *    @author    EIRL Timactive De Véra
+ *    @copyright Copyright (c) TIMACTIVE 2015 -EIRL Timactive De Véra
+ *    @version 1.1.2
+ *    @license   Commercial license
+ *
+ *************************************
+ **         HAPPY BIRTHDAY           *
+ **             V 1.1.0              *
+ *************************************
+ * +
+ * + Languages: EN, FR
+ * + PS version: 1.5
+ *
+ * Change log
+ *
+ ****** HappyBirthday 1.1.2
+ *
+ *      #3446 Prestashop 1.6 / error date birthday - to +
+ *      #3445 Prestashop 1.6 / little bit messed up for the number of days validity and discount 
+ *      #3439 Prestashop 1.6.0 / Integration bootstrap and error category 
+ *
+ ****** HappyBirthday 1.1.1
+ *
+ *      #3439 Prestashop 1.6.0 / Integration bootstrap and error category
+ *      
+ ****** HappyBirthday 1.1.0
+ *
+ *      #3432 1.1.0 / Version majeur / Envoi / Nombre de jour avant l'anniversaire 
+ *      
+ ****** HappyBirthday 1.0.2
+ *
+ *      - Bug Minor issue review Prestashop recommandation
+ *
+ ***** Happybirthday 1.0.1
+ *
+ *      - Add feature change doc include mail exception
+ *      - Add feature select lang in test form
+ *      - Bug Minor change cron url
+ *      - Bug Major in cron batch to log 
+ *      - Bug Minor logfiles is not a array
+ */
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Location: ../');
+exit;
+exit ();
